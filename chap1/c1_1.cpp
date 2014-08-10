288e0ad3c8447f8029df8e14e945fa44c4ec78d9
mryoshio <yoshiokaas@gmail.com>
2014-08-10 22:12:23 +0900

    update unique check

--- chap1/c1_1.cpp
+++ chap1/c1_1.cpp
@@ -1,37 +1,24 @@
-
-#include <vector>
 #include <iostream>
 
 using namespace std;
 
-bool dup(string arg) {
-    vector<int> nums;
-    //    cout << "--- start: " << arg << endl;
-    for (int i = 0; i < arg.size(); i++) {
-        if (find(nums.begin(), nums.end(), arg[i]) == nums.end()) {
-            nums.push_back(arg[i]);
-        } else {
-            //            std::vector<int>::iterator x = find(nums.begin(), nums.end(), arg[i]);
-            //            cout << "dup: " << arg[i] << " and " << (char)*x << endl;
-            return true;
-        }
-    }
-    return false;
+bool isUniqueChars(string s) {
+  int vs = 0, a = static_cast<int>('a');
+  for (int i = 0; i < s.size(); i++) {
+    int v = static_cast<int>(s[i]) - a;
+    if ((vs & (1 << v)) > 0)
+      return false;
+    vs |= (1 << v);
+  }
+  return true;
 }
 
-int main(){
-    vector<string> strs;
-    strs.push_back("shibata");
-    strs.push_back("shibaku");
-    vector<string>::iterator it = strs.begin();
-    while (it != strs.end()) {
-        cout << *it << " is ";
-        if (dup(*it)) {
-            cout << "true";
-        } else {
-            cout << "false";
-        }
-        cout << endl;
-        it++;
-    }
+/*
+  precondition: charcters are any of 'a-z'
+*/
+int main() {
+  string s;
+  cin >> s;
+  cout << isUniqueChars(s) << endl;
+  return 0;
 }
